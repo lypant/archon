e204d2b4991475eaca0db739db12bfa8c512f9c5
Adam Szczygieł <lypant@tlen.pl>
2014-08-07 19:43:19 +0200

    Remove obsolete variable

--- common/setup/all_steps.sh
+++ common/setup/all_steps.sh
@@ -1447,13 +1447,12 @@ copyProjectLogFiles()
 {
     req LOG_DIR $FUNCNAME
     req PROJECT_REPO_DST $FUNCNAME
-    req MACHINES_DIR_NAME $FUNCNAME
     req MACHINE $FUNCNAME
 
     # Do not perform typical logging in this function...
     # This would spoil nice logs copied to user's dir
 
-    cp -r $LOG_DIR $PROJECT_REPO_DST/$MACHINES_DIR_NAME/$MACHINE
+    cp -r $LOG_DIR $PROJECT_REPO_DST/$MACHINE
     err "$?" "$FUNCNAME" "failed to copy project log files"
 }
 
