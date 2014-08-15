4fcfc220c69e334ff846210404b8496888eec19b
Adam Szczygieł <lypant@tlen.pl>
2014-08-15 11:52:41 +0200

    First attempt to reorganize include order. Experiment with minimal variant.

--- common/setup/customize.sh
+++ common/setup/customize.sh
@@ -7,20 +7,34 @@
 # DESCRIPTION:  TODO: Describe
 #===============================================================================
 
+set -o nounset errexit
+
 #===============================================================================
 # Includes
 #===============================================================================
 
-source individual_settings.conf
-source ../../common/setup/project_settings.conf
+# Set variant name based on dir name
+MACHINE=$(cd ../; pwd)
+MACHINE=${MACHINE##*/}
+
+# Set variant name based on dir name
+PROJECT_NAME=$(cd ../../; pwd)
+PROJECT_NAME=${PROJECT_NAME##*/}
+
 source ../../common/setup/common_settings.conf
 source ../../common/setup/project_settings.conf
 source individual_settings.conf
-source ../../common/setup/functions.sh
-source ../../common/setup/helpers.sh
-source ../../common/setup/all_steps.sh
-source individual_steps.sh
-source ../../common/setup/common_steps.sh
+
+#source individual_settings.conf
+#source ../../common/setup/project_settings.conf
+#source ../../common/setup/common_settings.conf
+#source ../../common/setup/project_settings.conf
+#source individual_settings.conf
+#source ../../common/setup/functions.sh
+#source ../../common/setup/helpers.sh
+#source ../../common/setup/all_steps.sh
+#source individual_steps.sh
+#source ../../common/setup/common_steps.sh
 
 #===============================================================================
 #  Log file
