80e716b34d556d61876e08e611d38ade53d0723c
Adam Szczygieł <lypant@tlen.pl>
2014-08-28 16:27:18 +0200

    Move helper functions to functions file. Remove helpers file.

--- common/setup/customize.sh
+++ common/setup/customize.sh
@@ -25,7 +25,6 @@ source ../../common/setup/common_settings.conf
 source ../../common/setup/project_settings.conf
 source individual_settings.conf
 source ../../common/setup/functions.sh
-source ../../common/setup/helpers.sh
 source ../../common/setup/all_steps.sh
 source individual_steps.sh
 source ../../common/setup/common_steps.sh
